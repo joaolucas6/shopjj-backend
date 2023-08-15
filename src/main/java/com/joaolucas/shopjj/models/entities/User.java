eda53e36f20dbcb77172593dc2dc170b6fce8038
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-08-15 19:47:47 -0300

    feature: creating user fields and methods

--- src/main/java/com/joaolucas/shopjj/models/entities/User.java
+++ src/main/java/com/joaolucas/shopjj/models/entities/User.java
@@ -1,4 +1,180 @@
 package com.joaolucas.shopjj.models.entities;
 
+import com.joaolucas.shopjj.models.enums.Gender;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "cpf")
+    private String cpf;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @OneToOne
+    private ShoppingCart shoppingCart;
+
+    @OneToMany(mappedBy = "costumer")
+    private List<Order> orders = new ArrayList<>();
+
+    @OneToMany(mappedBy = "resident")
+    private List<Address> addresses = new ArrayList<>();
+
+    @OneToMany(mappedBy = "author")
+    private List<Review> reviews = new ArrayList<>();
+
+    public User(){
+
+    }
+
+    public User(Long id, String firstName, String lastName, String email, String password, String cpf, Gender gender, ShoppingCart shoppingCart) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.cpf = cpf;
+        this.gender = gender;
+        this.shoppingCart = shoppingCart;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public ShoppingCart getShoppingCart() {
+        return shoppingCart;
+    }
+
+    public void setShoppingCart(ShoppingCart shoppingCart) {
+        this.shoppingCart = shoppingCart;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    public List<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(List<Address> addresses) {
+        this.addresses = addresses;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        User user = (User) object;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", gender=" + gender +
+                ", shoppingCart=" + shoppingCart +
+                ", orders=" + orders +
+                ", addresses=" + addresses +
+                ", reviews=" + reviews +
+                '}';
+    }
 }
