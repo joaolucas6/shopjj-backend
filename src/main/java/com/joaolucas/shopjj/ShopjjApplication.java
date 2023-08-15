b002491b13296745bd9441247fe1720c61baf882
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-08-15 09:18:50 -0300

    feat: Initial commit

---
+++ src/main/java/com/joaolucas/shopjj/ShopjjApplication.java
@@ -0,0 +1,13 @@
+package com.joaolucas.shopjj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopjjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopjjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaolucas/shopjj/ShopjjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaolucas.shopjj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopjjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
