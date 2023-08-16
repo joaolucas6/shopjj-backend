ac4615fcc71a304521bc79df0638824cbf891679
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-08-16 16:00:16 -0300

    feat: creating order entity

--- src/main/java/com/joaolucas/shopjj/models/entities/Order.java
+++ src/main/java/com/joaolucas/shopjj/models/entities/Order.java
@@ -1,4 +1,146 @@
 package com.joaolucas.shopjj.models.entities;
 
+import com.joaolucas.shopjj.models.enums.OrderStatus;
+import com.joaolucas.shopjj.models.enums.PaymentMethod;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order")
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+
+    @Column(name = "total_price")
+    private BigDecimal totalPrice;
+
+    @Column(name = "address")
+    private Address address;
+
+    @Column(name = "instant")
+    private LocalDateTime instant;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    @ElementCollection
+    private HashMap<Integer, Product> products = new HashMap<>();
+
+    public Order(){
+
+    }
+
+    public Order(Long id, User client, BigDecimal totalPrice, Address address, LocalDateTime instant, PaymentMethod paymentMethod, OrderStatus orderStatus) {
+        this.id = id;
+        this.client = client;
+        this.totalPrice = totalPrice;
+        this.address = address;
+        this.instant = instant;
+        this.paymentMethod = paymentMethod;
+        this.orderStatus = orderStatus;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public LocalDateTime getInstant() {
+        return instant;
+    }
+
+    public void setInstant(LocalDateTime instant) {
+        this.instant = instant;
+    }
+
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public HashMap<Integer, Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(HashMap<Integer, Product> products) {
+        this.products = products;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Order order = (Order) object;
+        return Objects.equals(id, order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", client=" + client +
+                ", totalPrice=" + totalPrice +
+                ", address=" + address +
+                ", instant=" + instant +
+                ", paymentMethod=" + paymentMethod +
+                ", orderStatus=" + orderStatus +
+                ", products=" + products +
+                '}';
+    }
 }
