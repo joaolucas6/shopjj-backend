f75cd4b18511641cf3dfb4a59c9f3612e9612ab5
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-08-16 16:05:08 -0300

    feat: creating coupon entity

--- src/main/java/com/joaolucas/shopjj/models/entities/Coupon.java
+++ src/main/java/com/joaolucas/shopjj/models/entities/Coupon.java
@@ -1,4 +1,91 @@
 package com.joaolucas.shopjj.models.entities;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_coupon")
 public class Coupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "validity")
+    private LocalDateTime validity;
+
+    public Coupon(){
+
+    }
+
+    public Coupon(Long id, String name, String description, LocalDateTime validity) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.validity = validity;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getValidity() {
+        return validity;
+    }
+
+    public void setValidity(LocalDateTime validity) {
+        this.validity = validity;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Coupon coupon = (Coupon) object;
+        return Objects.equals(id, coupon.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Coupon{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", validity=" + validity +
+                '}';
+    }
 }
