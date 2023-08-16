0f16c7368fb2596e81e4149fb17031aa030a66fd
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-08-16 16:19:40 -0300

    feat: creating payment method enum

--- src/main/java/com/joaolucas/shopjj/models/enums/PaymentMethod.java
+++ src/main/java/com/joaolucas/shopjj/models/enums/PaymentMethod.java
@@ -1,4 +1,11 @@
 package com.joaolucas.shopjj.models.enums;
 
 public enum PaymentMethod {
+    CREDIT_CARD,
+    DEBIT_CARD,
+    PAYPAL,
+    APPLE_PAY,
+    GOOGLE_PAY,
+    CASH,
+    BANK_TRANSFER
 }
