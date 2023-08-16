78e1febb68b4611cf137d31adf246c37fcdb9117
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-08-16 15:47:55 -0300

    feat: creating product entity

--- src/main/java/com/joaolucas/shopjj/models/entities/Product.java
+++ src/main/java/com/joaolucas/shopjj/models/entities/Product.java
@@ -1,4 +1,154 @@
 package com.joaolucas.shopjj.models.entities;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_product")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @Column(name = "availableQuantity")
+    private Integer availableQuantity;
+
+    @ManyToMany(mappedBy = "products")
+    private List<Category> categories = new ArrayList<>();
+
+    @OneToMany(mappedBy = "product")
+    private List<Review> reviews = new ArrayList<>();
+
+    @OneToMany(mappedBy = "product")
+    private List<Promotion> promotions =  new ArrayList<>();
+
+    @OneToMany(mappedBy = "product")
+    private List<Coupon> coupons = new ArrayList<>();
+
+    public Product (){
+
+    }
+
+    public Product(Long id, String description, String imageUrl, BigDecimal price, Integer availableQuantity) {
+        this.id = id;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.price = price;
+        this.availableQuantity = availableQuantity;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getAvailableQuantity() {
+        return availableQuantity;
+    }
+
+    public void setAvailableQuantity(Integer availableQuantity) {
+        this.availableQuantity = availableQuantity;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    public List<Promotion> getPromotions() {
+        return promotions;
+    }
+
+    public void setPromotions(List<Promotion> promotions) {
+        this.promotions = promotions;
+    }
+
+    public List<Coupon> getCoupons() {
+        return coupons;
+    }
+
+    public void setCoupons(List<Coupon> coupons) {
+        this.coupons = coupons;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Product product = (Product) object;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", price=" + price +
+                ", availableQuantity=" + availableQuantity +
+                ", categories=" + categories +
+                ", reviews=" + reviews +
+                ", promotions=" + promotions +
+                ", coupons=" + coupons +
+                '}';
+    }
 }
