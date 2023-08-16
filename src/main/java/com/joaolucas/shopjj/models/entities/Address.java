82ae2c22b94dc48a49ff479c020605dfea4cfe63
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-08-16 16:17:54 -0300

    feat: creating address entity

--- src/main/java/com/joaolucas/shopjj/models/entities/Address.java
+++ src/main/java/com/joaolucas/shopjj/models/entities/Address.java
@@ -1,4 +1,142 @@
 package com.joaolucas.shopjj.models.entities;
 
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_address")
 public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "number")
+    private Integer number;
+
+    @Column(name = "complement")
+    private String complement;
+
+    @Column(name = "cep")
+    private String cep;
+
+    @ManyToOne
+    @JoinColumn(name = "resident_id")
+    private User resident;
+
+    public Address(){
+
+    }
+
+    public Address(Long id, String state, String city, String street, Integer number, String complement, String cep, User resident) {
+        this.id = id;
+        this.state = state;
+        this.city = city;
+        this.street = street;
+        this.number = number;
+        this.complement = complement;
+        this.cep = cep;
+        this.resident = resident;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getComplement() {
+        return complement;
+    }
+
+    public void setComplement(String complement) {
+        this.complement = complement;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public User getResident() {
+        return resident;
+    }
+
+    public void setResident(User resident) {
+        this.resident = resident;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Address address = (Address) object;
+        return Objects.equals(id, address.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "id=" + id +
+                ", state='" + state + '\'' +
+                ", city='" + city + '\'' +
+                ", street='" + street + '\'' +
+                ", number=" + number +
+                ", complement='" + complement + '\'' +
+                ", cep='" + cep + '\'' +
+                ", resident=" + resident +
+                '}';
+    }
 }
