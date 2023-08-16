3d6492e0e6c6283c423a7c4cd5781396077f9223
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-08-16 15:20:20 -0300

    feat: creating shopping cart entity

--- src/main/java/com/joaolucas/shopjj/models/entities/ShoppingCart.java
+++ src/main/java/com/joaolucas/shopjj/models/entities/ShoppingCart.java
@@ -1,4 +1,79 @@
 package com.joaolucas.shopjj.models.entities;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_shopping_cart")
 public class ShoppingCart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private User costumer;
+
+    @ElementCollection
+    private List<Product> products = new ArrayList<>();
+
+    public ShoppingCart(){
+
+    }
+
+    public ShoppingCart(Long id, User costumer) {
+        this.id = id;
+        this.costumer = costumer;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getCostumer() {
+        return costumer;
+    }
+
+    public void setCostumer(User costumer) {
+        this.costumer = costumer;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        ShoppingCart that = (ShoppingCart) object;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "ShoppingCart{" +
+                "id=" + id +
+                ", costumer=" + costumer +
+                ", products=" + products +
+                '}';
+    }
+
+
 }
