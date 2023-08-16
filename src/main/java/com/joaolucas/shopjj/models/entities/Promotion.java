d15c54296f047a89e6d8f31e10f0faa94f0d40e7
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-08-16 15:30:30 -0300

    feat: creating promotion entity

--- src/main/java/com/joaolucas/shopjj/models/entities/Promotion.java
+++ src/main/java/com/joaolucas/shopjj/models/entities/Promotion.java
@@ -1,4 +1,106 @@
 package com.joaolucas.shopjj.models.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_promotion")
 public class Promotion {
+
+    private Long id;
+    private String description;
+    private Double percentage;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private List<Product> products = new ArrayList<>();
+
+    public Promotion(){
+
+    }
+
+    public Promotion(Long id, String description, Double percentage, LocalDateTime startDate, LocalDateTime endDate) {
+        this.id = id;
+        this.description = description;
+        this.percentage = percentage;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(Double percentage) {
+        this.percentage = percentage;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Promotion promotion = (Promotion) object;
+        return Objects.equals(id, promotion.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Promotion{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", percentage=" + percentage +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                ", products=" + products +
+                '}';
+    }
 }
