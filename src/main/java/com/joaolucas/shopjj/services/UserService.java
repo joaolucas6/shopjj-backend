29a82b391333059eb52725ded23593b7acd31ff8
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-08-20 19:24:37 -0300

    feat: creating UserService

---
+++ src/main/java/com/joaolucas/shopjj/services/UserService.java
@@ -0,0 +1,42 @@
+package com.joaolucas.shopjj.services;
+
+import com.joaolucas.shopjj.models.dto.UserDTO;
+import com.joaolucas.shopjj.models.entities.User;
+import com.joaolucas.shopjj.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserDTO create(UserDTO userDTO){
+        User user = new User();
+
+        user.setFirstName(userDTO.getFirstName());
+        user.setLastName(userDTO.getLastName());
+        user.setEmail(userDTO.getEmail());
+        user.setCpf(userDTO.getCpf());
+        user.setGender(userDTO.getGender());
+
+        return new UserDTO(userRepository.save(user));
+    }
+
+    public UserDTO update(Long id, UserDTO updateRequest){
+        User user = userRepository.findById(id).orElseThrow();
+
+        if(updateRequest.getFirstName() != null) updateRequest.setFirstName(updateRequest.getFirstName());
+        if(updateRequest.getLastName() != null) updateRequest.setLastName(updateRequest.getLastName());
+        if(updateRequest.getEmail() != null) updateRequest.setEmail(updateRequest.getEmail());
+        if(updateRequest.getGender() != null) updateRequest.setGender(updateRequest.getGender());
+
+        return new UserDTO(userRepository.save(user));
+    }
+
+    public void delete(Long id){
+        User user = userRepository.findById(id).orElseThrow();
+        userRepository.delete(user);
+    }
+
+    
+}
