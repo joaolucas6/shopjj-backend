f8118da4b2914630f2157d81070f6252b7badcd9
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-09-02 09:36:48 -0300

    feat: creating UserController

---
+++ src/main/java/com/joaolucas/shopjj/controllers/UserController.java
@@ -0,0 +1,43 @@
+package com.joaolucas.shopjj.controllers;
+
+import com.joaolucas.shopjj.models.dto.UserDTO;
+import com.joaolucas.shopjj.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> findAll(){
+        return ResponseEntity.ok(userService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable Long id){
+        return ResponseEntity.ok(userService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> create(@RequestBody UserDTO userDTO){
+        return ResponseEntity.ok(userService.create(userDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> update(@PathVariable Long id, @RequestBody UserDTO userDTO){
+        return ResponseEntity.ok(userService.update(id, userDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        userService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+}
