7bfd6a304c639044c0bfe23c1b6f2e11e2f759a2
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-09-02 18:15:36 -0300

    feat: implementing hateoas at AddressService

--- src/main/java/com/joaolucas/shopjj/services/AddressService.java
+++ src/main/java/com/joaolucas/shopjj/services/AddressService.java
@@ -1,5 +1,6 @@
 package com.joaolucas.shopjj.services;
 
+import com.joaolucas.shopjj.controllers.AddressController;
 import com.joaolucas.shopjj.exceptions.BadRequestException;
 import com.joaolucas.shopjj.exceptions.ResourceNotFoundException;
 import com.joaolucas.shopjj.models.dto.AddressDTO;
@@ -13,6 +14,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @Service
 @RequiredArgsConstructor
 public class AddressService {
@@ -20,12 +24,14 @@ public class AddressService {
     private final AddressRepository addressRepository;
     private final UserRepository userRepository;
 
+
+
     public List<AddressDTO> findAll(){
-        return addressRepository.findAll().stream().map(AddressDTO::new).toList();
+        return addressRepository.findAll().stream().map(address -> new AddressDTO(address).add(linkTo(methodOn(AddressController.class).findById(address.getId())).withSelfRel())).toList();
     }
 
     public AddressDTO findById(Long id){
-        return new AddressDTO(addressRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Address was not found with ID: " + id)));
+        return new AddressDTO(addressRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Address was not found with ID: " + id))).add(linkTo(methodOn(AddressController.class).findById(id)).withSelfRel());
     }
 
     public AddressDTO create(Long residentId, AddressDTO addressDTO){
@@ -42,7 +48,9 @@ public class AddressService {
         address.setComplement(addressDTO.getComplement());
         address.setCep(addressDTO.getCep());
 
-        return new AddressDTO(addressRepository.save(address));
+        Address savedAddress = addressRepository.save(address);
+
+        return new AddressDTO(savedAddress).add(linkTo(methodOn(AddressController.class).findById(savedAddress.getId())).withSelfRel());
     }
 
     public AddressDTO update(Long id, AddressDTO addressDTO){
@@ -56,7 +64,7 @@ public class AddressService {
         if(addressDTO.getComplement() != null) address.setComplement(addressDTO.getComplement());
         if(addressDTO.getCep() != null) address.setCep(addressDTO.getCep());
 
-        return new AddressDTO(addressRepository.save(address));
+        return new AddressDTO(addressRepository.save(address)).add(linkTo(methodOn(AddressController.class).findById(id)).withSelfRel());
     }
 
     public void delete(Long id){
