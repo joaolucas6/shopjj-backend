5a4b076fbb16aba225c9c8a80f700dbe5f047188
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-09-02 10:02:38 -0300

    feat: creating PromotionController

---
+++ src/main/java/com/joaolucas/shopjj/controllers/PromotionController.java
@@ -0,0 +1,45 @@
+package com.joaolucas.shopjj.controllers;
+
+import com.joaolucas.shopjj.models.dto.PromotionDTO;
+import com.joaolucas.shopjj.services.PromotionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/promotions")
+@RequiredArgsConstructor
+public class PromotionController {
+
+    private final PromotionService promotionService;
+
+    @GetMapping
+    public ResponseEntity<List<PromotionDTO>> findAll(){
+        return ResponseEntity.ok(promotionService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PromotionDTO> findById(@PathVariable Long id){
+        return ResponseEntity.ok(promotionService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<PromotionDTO> create(@RequestBody PromotionDTO promotionDTO){
+        return ResponseEntity.ok(promotionService.create(promotionDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PromotionDTO> update(@PathVariable Long id, @RequestBody PromotionDTO promotionDTO){
+        return ResponseEntity.ok(promotionService.update(id, promotionDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        promotionService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
+    // add item  remove item
+}
