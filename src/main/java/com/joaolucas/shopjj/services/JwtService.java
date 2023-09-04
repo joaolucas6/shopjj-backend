0be57e19085cd631ca823d7c0a41c3dd0ded991d
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-09-04 16:05:24 -0300

    feat: creating JwtService

---
+++ src/main/java/com/joaolucas/shopjj/services/JwtService.java
@@ -0,0 +1,38 @@
+package com.joaolucas.shopjj.services;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.joaolucas.shopjj.models.entities.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+@RequiredArgsConstructor
+public class JwtService {
+
+    @Value("${application.security.jwt.secret-key}")
+    private String SECRET_KEY;
+
+    @Value("${application.security.jwt.expiration}")
+    private long expiration;
+
+    public String generateToken(User user){
+        return JWT
+                .create()
+                .withSubject(user.getUsername())
+                .withIssuedAt(new Date(System.currentTimeMillis()))
+                .withExpiresAt(new Date(System.currentTimeMillis() + expiration))
+                .sign(Algorithm.HMAC256(SECRET_KEY));
+    }
+
+    public String validateToken(String token){
+        return JWT
+                .require(Algorithm.HMAC256(SECRET_KEY))
+                .build()
+                .verify(token)
+                .getSubject();
+    }
+}
